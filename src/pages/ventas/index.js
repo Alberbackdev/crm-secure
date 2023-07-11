dc96fd06a560396f4b7d2765f3570f7e6d2e37fb
BricMichael <bricmichael@gmail.com>
2023-07-11 12:58:08 -0400

    Form Difunto | Ventas

--- src/pages/ventas/index.js
+++ src/pages/ventas/index.js
@@ -1,10 +1,17 @@
 import Layout from "../../components/LayoutFolder/Layout"
+import FormDifunto from "../../components/ventas/FormDifunto/FormDifunto"
+import NumerosServicios from "../../components/ventas/NumeroServicios/NumerosServicios"
 
 //registar /actualizar venta
 
 
-export default function Home() {
-    return <Layout>
-        Ventas
-    </Layout>
+export default function ventas() {
+    return (
+        <Layout>
+            <div style={{display: "flex", width: "100%", marginTop: '3rem', justifyContent: 'space-evenly'}}> 
+                <FormDifunto />
+                <NumerosServicios />
+            </div>
+        </Layout> 
+    )
 }

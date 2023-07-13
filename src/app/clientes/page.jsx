33b6c78f46381a0d87318e39e8499016ff3fbc97
Alberbackdev <albertograterolca@gmail.com>
2023-07-13 01:00:15 -0400

    orden y limpieza1

--- src/app/clientes/page.jsx
+++ src/app/clientes/page.jsx
@@ -1,17 +1,17 @@
-import ButtonsActions from "../../components/listaClientes/buttonsActions/buttonsActions"
-import Listado from "../../components/listaClientes/listado/Listado"
+
+import ButtonsActions from "@/src/components/listaClientes/buttonsActions/buttonsActions";
+import Listado from "@/src/components/listaClientes/listado/Listado";
 
 
 //This is form for new client
 
 export default function lista() {
-
-    return (
-        <>
-            <div style={{ marginLeft: '41px', marginTop: '3rem' }}>
-                <ButtonsActions />
-                <Listado />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div style={{ marginLeft: "41px", marginTop: "3rem" }}>
+        <ButtonsActions />
+        <Listado />
+      </div>
+    </>
+  );
 }

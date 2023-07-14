3761180c07ced06af0cf426824b30cbfe588fd19
Alberbackdev <albertograterolca@gmail.com>
2023-07-13 22:04:35 -0400

    next13 app/1

--- src/utils/namePage.js
+++ src/utils/namePage.js
@@ -7,4 +7,5 @@ export const NamePageHelper = () => {
     if(pathname === '/dashboard') return 'Dashboard';
     if(pathname === '/ventas') return 'Ventas';
     if (pathname === '/login') return 'Inicio de Sesión';
+    if (pathname === '/register') return 'Registrate';
 }

9d5f3ba4420123abf29b46d7a3927acfe8f3f656
BricMichael <bricmichael@gmail.com>
2023-09-22 16:42:34 -0400

    Evento de navegacion | Barra de progreso

--- src/components/BarSeguimientoProceso/BarSeguimientoProceso.jsx
+++ src/components/BarSeguimientoProceso/BarSeguimientoProceso.jsx
@@ -1,22 +1,62 @@
-import Image from 'next/image'
-import style from './BarSeguimientoProceso.module.css'
+"use client"
+import Image from 'next/image';
+import style from './BarSeguimientoProceso.module.css';
+import { useRouter } from "next/navigation";
 
-const namesPageVentas = ['Datos Del Difunto', 'Datos Responsable del Servicio', 'Servicio', 'Confirmacion'];
-const namesPageClientes = ['Datos Principales', 'Poliza', 'Pago', 'Confirmacion'];
+
+const namesPageVentas = [
+    {
+        namePage: 'Datos Del Difunto',
+        urlPage: '/ventas'
+    }, 
+    {
+        namePage: 'Datos Responsable del Servicio',
+        urlPage: '/ventas/responsable'
+    }, 
+    {
+        namePage: 'Servicio',
+        urlPage: '/ventas/servicio'
+    }, 
+    {
+        namePage: 'Confirmacion',
+        urlPage: '/ventas/confirmar'
+    }
+];
+const namesPageClientes = [
+    {
+        namePage: 'Datos Principales',
+        urlPage: '/clientes'
+    }, 
+    {
+        namePage: 'Poliza',
+        urlPage: '/clientes/poliza'
+    }, 
+    {
+        namePage: 'Pago',
+        urlPage: '/clientes/pago'
+    }, 
+    {
+        namePage: 'Confirmacion',
+        urlPage: '/clientes/confirmar'
+    }
+]
+
 
 function BarSeguimientoProceso({currentPage, useBarTo}) {
   const listaARecorrer = useBarTo === 'Ventas' ? namesPageVentas : namesPageClientes;
-  const indexCurrentPage = listaARecorrer.indexOf(currentPage)
+  const indexCurrentPage = listaARecorrer.map(page => page.namePage).indexOf(currentPage);
+  const router = useRouter();
+
   return (
     <div className={style.content}>
         {
-            listaARecorrer.map((namePage, index) => (
+            listaARecorrer.map((page, index) => (
                 <>
-                    <div className={style.process} key={namePage}> 
+                    <div className={style.process} key={page.namePage} onClick={() => router.push(page.urlPage)}>
                         <div className={`${style.circleIndicator} ${index <= indexCurrentPage ? style.circleIndicatorActive : null}`}>
                             <Image src='/checkWhiteIcon.png' alt='check White Icon' width={16} height={13} />
                         </div>
-                        <p className={`${index <= indexCurrentPage ? style.nameActive : style.nameInactive}`}>{namePage}</p>
+                        <p className={`${index <= indexCurrentPage ? style.nameActive : style.nameInactive}`}>{page.namePage}</p>
                     </div>
                     { index < 3 && <div className={style.separator}></div>}
                 </>

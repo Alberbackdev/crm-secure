f5548992d1a2ed1093fc5ca60ae47ed32fe95109
Alberbackdev <58196572+Alberbackdev@users.noreply.github.com>
2023-10-31 19:57:02 -0400

    Demo update

--- src/app/api/clientes/beneficiarios/route.js
+++ src/app/api/clientes/beneficiarios/route.js
@@ -3,31 +3,29 @@ import BeneficiarioData from "@/src/models/BeneficiarioData";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
-
 export async function GET(req, res) {
-    await connectDB()
-    const beneficiarios = await BeneficiarioData.find().populate('cliente');
-    return NextResponse.json(beneficiarios, { status: 200 })
+  await connectDB();
+  const beneficiarios = await BeneficiarioData.find().populate("cliente");
+  return NextResponse.json(beneficiarios, { status: 200 });
 }
 
 export async function POST(req, res) {
-    await connectDB()
-    try {
-        const body = await req.json()
-        const newDataBeneficiario = await BeneficiarioData.create(body)
-        return NextResponse.json({ data: newDataBeneficiario }, { status: 201 })
-    } catch (error) {
-        if (error instanceof mongoose.Error.ValidationError) {
-            return NextResponse.json(
-                {
-                    message: error.message
-                },
-                {
-                    status: 400
-                }
-            )
+  await connectDB();
+  try {
+    const body = await req.json();
+    const newDataBeneficiario = await BeneficiarioData.create(body);
+    return NextResponse.json({ data: newDataBeneficiario }, { status: 201 });
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        {
+          status: 400,
         }
-        return NextResponse.error()
+      );
     }
-
+    return NextResponse.error();
+  }
 }

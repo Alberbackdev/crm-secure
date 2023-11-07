79aba413c01931615748c22ed0ad907d54e1e0fd
BricMichael <bricmichael@gmail.com>
2023-11-07 17:19:24 -0400

    Cambiar clave | Config Responsive

--- src/app/configuracion/page.jsx
+++ src/app/configuracion/page.jsx
@@ -2,20 +2,13 @@
 import PreferenciasPersonales from "@/src/components/Ajustes/PreferenciasPersonales/PreferenciasPersonales"
 import ContainerPrinc from "../../components/LayoutFolder/Layout";
 import MenuConfig from "@/src/components/MenuConfiguracion/MenuConfig"
-
+import style from '@/src/styles/stylePages.module.css'
 
 export default function Configuracion() {
 
     return (
         <ContainerPrinc>
-            <div
-                style={{
-                    display: "flex",
-                    width: "100%",
-                    marginTop: "1.7rem",
-                    justifyContent: "space-evenly",
-                }}
-            >
+            <div className={style.configPage}>
                <MenuConfig />
                <PreferenciasPersonales />
             </div>

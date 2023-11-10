c630140ad8513bd7cc7f4445201f8fc21878f4a3
Alberbackdev <58196572+Alberbackdev@users.noreply.github.com>
2023-11-10 18:09:54 -0400

    clientes-pagos lista1.1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,44 +19,51 @@ export default function Header() {
     <div className={style.header}>
       <div className={style.headerNamePage}>
         <h2 className={style.namePage}>{NamePageHelper(pathname)}</h2>
-        { (pathname.length > 1 && !isMobile) 
-            ? <p className={style.pathPage}>{pathname}</p>
-            :
-              <button type='button' onClick={() => router.push('/clientes/cliente')}>
-                  <Image
-                      priority
-                      src="/add.png"
-                      height={24}
-                      width={24}
-                      alt="Follow us on Twitter"
-                  />
-                  Registrar
-              </button>
-        }
+        {pathname.length > 1 && !isMobile ? (
+          <p className={style.pathPage}>{pathname}</p>
+        ) : (
+          <button
+            type="button"
+            onClick={() => router.push("/clientes/cliente")}
+          >
+            <Image
+              priority
+              src="/add.png"
+              height={24}
+              width={24}
+              alt="Follow us on Twitter"
+            />
+            Registrar
+          </button>
+        )}
       </div>
-      
-        {/* !isMobile &&  */}
-          <div className={style.contentAvatar}>
-            <div className={style.circle}>
-              <Image
-                src="/favicon.ico"
-                alt="Profile Image"
-                className={style.avatar}
-                width={50}
-                height={50}
-              />
-            </div>
-            <div className={style.nameAndRole}>
-              <p className="px-2">{session?.user?.name}</p>
-              <p
-                className="px-2"
-                style={{ marginTop: "-5px", fontSize: "14px", color: "#00000068" }}
-              >
-                Secretario
-              </p>
-            </div>
+
+      {!isMobile && (
+        <div className={style.contentAvatar}>
+          <div className={style.circle}>
+            <Image
+              src="/favicon.ico"
+              alt="Profile Image"
+              className={style.avatar}
+              width={50}
+              height={50}
+            />
           </div>
-      
+          <div className={style.nameAndRole}>
+            <p className="px-2">{session?.user?.name}</p>
+            <p
+              className="px-2"
+              style={{
+                marginTop: "-5px",
+                fontSize: "14px",
+                color: "#00000068",
+              }}
+            >
+              Secretario
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

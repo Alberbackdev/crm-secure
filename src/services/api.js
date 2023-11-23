4956e4e2ea21b1a5febb796e832a64360570a4a0
Alberbackdev <58196572+Alberbackdev@users.noreply.github.com>
2023-11-22 21:23:32 -0400

    Url Api Dev

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const { NEXTAUTH_URL } = process.env;
-export const urlApiDev = `${NEXTAUTH_URL}/api`;
+//const { NEXTAUTH_URL } = process.env;
+export const urlApiDev = "https://crm-secure.vercel.app/api";
 /* export const urlApiProd = ; */
 
 //export const urlApiDev = "http://localhost:3000/api";

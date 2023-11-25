f3babd8432eff674fadd739bdb2c8638016d5446
Alberbackdev <58196572+Alberbackdev@users.noreply.github.com>
2023-11-24 23:39:40 -0400

    urlapidev apunta a vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
-//const { NEXTAUTH_URL } = process.env;
-//export const urlApiDev = "https://crm-secure.vercel.app/api";
-/* export const urlApiProd = ; */
 
-export const urlApiDev = "http://localhost:3000/api";
-//export const urlApiProd = "https://api.example.com";
+export const urlApiDev = "https://crm-secure.vercel.app/api";
+
+//export const urlApiDev = "http://localhost:3000/api";
+
